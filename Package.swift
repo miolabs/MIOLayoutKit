854e6cf03d0535f442bb1952b97a1759c9e3b53d
Javier Segura <javier@miolabs.com>
2026-08-25 14:51:18 +0200

    Point package dependencies to develop

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
             targets: ["MIOLayoutKit"]),
     ],
     dependencies: [
-        .package( url: "https://github.com/miolabs/MIOCore.git", branch: "master" ),
-        .package( url: "https://github.com/miolabs/PDFLib-Swift.git", branch: "main" )
+        .package( url: "https://github.com/miolabs/MIOCore.git", branch: "develop" ),
+        .package( url: "https://github.com/miolabs/PDFLib-Swift.git", branch: "develop" )
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
